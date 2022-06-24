27b63a3c774eaba146232c1cdbc11493ebbf413c
Julien-ser <73262183+Julien-ser@users.noreply.github.com>
2022-06-24 18:40:10 -0400

    Add files via upload

--- testing lol.py
+++ testing lol.py
@@ -14,7 +14,7 @@ def gpt3(stext):
     return content
 
 def generate(char1, char2, nsfw):
-    openai.api_key = "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"
+    openai.api_key = "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"
     print(str(nsfw))
     if str(nsfw) == "True":
         response = gpt3("Write a nsfw fanfiction for " + char1 + " and  " + char2)
